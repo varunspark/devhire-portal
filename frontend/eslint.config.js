b1cb28d803f40f44888f39f43bd551bcb4a600a4
varun <varunsparked@gmail.com>
2026-07-21 13:51:47 +0530

    Add ESLint config and wire lint stage into CI pipeline

---
+++ frontend/eslint.config.js
@@ -0,0 +1,36 @@
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['src/**/*.{js,jsx}'],
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+    },
+    languageOptions: {
+      ecmaVersion: 2024,
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'no-unused-vars': 'warn',
+      'react-hooks/set-state-in-effect': 'warn',
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+  },
+];
